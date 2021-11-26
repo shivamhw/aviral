558c53771439fcb7a60634e485501f0acc8877e9
shivamhw <s.k.mishra703@gmail.com>
2021-11-27 00:19:06 +0530

    renamed pkgname

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
   name = 'aviral',         # How you named your package folder (MyLib)
-  packages = ['aviral'],   # Chose the same as "name"
+  packages = ['aviral_api'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'api wrapper for IIIT Allahabad aviral portal',   # Give a short description about your library
@@ -11,8 +11,7 @@ setup(
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'requests',
-          'json',
+          'requests'
       ],
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
