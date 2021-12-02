c124cbaaa6cde71001f51861d3c976aa013cd807
shivamhw <s.k.mishra703@gmail.com>
2021-12-02 20:50:01 +0530

    Pypi support added

--- setup.py
+++ setup.py
@@ -1,24 +1,29 @@
 from distutils.core import setup
+from os import read
+
+readme = open('README', 'r')
+README = readme.read()
 setup(
-  name = 'aviral',         # How you named your package folder (MyLib)
-  packages = ['aviral_api'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'api wrapper for IIIT Allahabad aviral portal',   # Give a short description about your library
-  author = 'Shivam Mishra',                   # Type in your name
-  author_email = 'shivamhw0@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  name = 'aviral',        
+  packages = ['aviral_api'],   
+  version = '0.1.2',      
+  license='MIT',        
+  description = 'Api wrapper for IIIT Allahabad aviral portal',   
+  long_description_content_type = "text/markdown",
+  long_description =README,   
+  author = 'Shivam Mishra',                   
+  author_email = 'shivamhw0@gmail.com',      
+  url = 'https://github.com/shivamhw/aviral',   
+  keywords = ['IIITA', 'Aviral', 'IIIT-A'],   
+  install_requires=[            
           'requests'
       ],
   classifiers=[
-    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 4 - Beta',    
+    'Intended Audience :: Developers',    
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3.9',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3.9',   
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
